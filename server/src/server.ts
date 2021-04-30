0f9b2b7030f6073346c42cc670eba7d18c0e8ce5
Yasser Ehab <56257332+Yasser-dev@users.noreply.github.com>
2021-04-30 15:53:34 +0800

    register user route

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,12 +3,18 @@ import { createConnection } from "typeorm";
 import express from "express";
 import morgan from "morgan";
 
+import authRoutes from "./routes/auth";
+
+import trim from "./middlewares/trim";
+
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(trim);
 
 app.get("/", (_, res) => res.send("Hello World"));
+app.use("/api/auth", authRoutes);
 
 app.listen(5000, async () => {
   console.log("Server running at http://localhost:5000");
